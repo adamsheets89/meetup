6cfe7d470704da78cc14bb616e84c6c92ec8229c
Adam Sheets <asheets@mavens.com>
2021-09-01 17:48:08 -0500

    added search box, pagereference handling

--- meetup/force-app/main/default/lwc/registrationPage/registrationPage.js
+++ meetup/force-app/main/default/lwc/registrationPage/registrationPage.js
@@ -1,36 +1,85 @@
-import { NavigationMixin } from 'lightning/navigation';
+import getMeetup from '@salesforce/apex/RegistrationPageController.getMeetup'
+import { CurrentPageReference, NavigationMixin } from 'lightning/navigation';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { api, LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 
 const HEADER = 'Meetup Registration';
-const FIELDS = [
-  {name: 'FirstName__c', value: null},
-  {name: 'LastName__c', value: null},
-  {name: 'Email__c', value: null}
-];
+const SEARCH_LABEL = 'Provide the registration code for the meetup you wish to attend';
+const FIELDS = ['FirstName__c', 'LastName__c', 'Email__c'];
+
+const INVALID_CODE_ERROR_MESSAGE = 'The provided registration code is invalid.';
 
 export default class RegistrationPage extends NavigationMixin(LightningElement) {
-  @api code;
-  @track meetupId;
+  @track registrationCode;
+  @track meetup;
+  @track registrationFields;
   @track isRegistering = false;
 
-  @api
-  set recordId(recordId) {
-    this.meetupId = recordId;
-    FIELDS.unshift({name: 'Meetup__c', value: recordId});
-  }
-
-  get recordId() {
-    return this.meetupId;
-  }
+  currentPageReference;
 
   get header() {
     return HEADER;
   }
 
-  get fields() {
-    return FIELDS;
+  get searchLabel() {
+    return SEARCH_LABEL;
   }
+
+  get disableSearch() {
+    return !this.registrationCode;
+  }
+
+  get showForm() {
+    return this.meetup && this.registrationFields;
+  }
+
+  connectedCallback() {
+    window.addEventListener("popstate", () => {
+      this.resetState();
+    });
+  }
+
+  @wire(CurrentPageReference)
+  setCurrentPageReference(currentPageReference) {
+    this.currentPageReference = currentPageReference;
+    if (this.currentPageReference.state.c__code) {
+      this.initializeData();
+    };
+  }
+
+  goToForm() {
+    this.isRegistering = true;
+    const updatedPageReference = this.getUpdatedPageReference({ c__code: this.registrationCode });
+    this[NavigationMixin.Navigate](updatedPageReference, true);      
+  }
+
+  async initializeData() {
+    try {
+      this.meetup = await getMeetup({ registrationCode: this.currentPageReference.state.c__code });
+      if (!this.meetup){
+        return this.showToast(INVALID_CODE_ERROR_MESSAGE, 'error');
+      }
+      this.registrationFields = FIELDS.map(f => ({name: f, value: undefined}));
+      this.registrationFields.unshift({name: 'Meetup__c', value: this.meetup.Id});
+    } catch (error) {
+      this.showToast(error, 'error');
+    } finally {
+      this.isRegistering = false;
+    }
+  }
+
+  getUpdatedPageReference(stateChanges) {
+    return Object.assign({}, this.currentPageReference, {
+        state: Object.assign({}, this.currentPageReference.state, stateChanges)
+    });
+  }
+
+  handleChange(event){
+    const input = event.target.value;
+    if (input.length === 8) {
+      this.registrationCode = input;
+    }
+  } 
 
   handleSubmit() {
     this.isRegistering = true;
@@ -40,6 +89,7 @@ export default class RegistrationPage extends NavigationMixin(LightningElement) 
     this.isRegistering = false;
     this.showToast('Successfully registered', 'success');
     this.returnToMeetup();
+    this.resetState();
   }
 
   handleError(event) {
@@ -51,7 +101,7 @@ export default class RegistrationPage extends NavigationMixin(LightningElement) 
     this[NavigationMixin.Navigate]({
       type: 'standard__recordPage',
       attributes: {
-        recordId: this.recordId,
+        recordId: this.meetup.Id,
         actionName: 'view',
       },
     });
@@ -64,5 +114,11 @@ export default class RegistrationPage extends NavigationMixin(LightningElement) 
         variant
       })
     );
+  }
+
+  resetState() {
+    this.registrationCode = undefined;
+    this.meetup = undefined;
+    this.registrationFields = undefined;
   }
 }
